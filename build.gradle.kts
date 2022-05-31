02ecb6980219467f001c2a9e63106b2b05585a4a
Kenneth Stigen <kenneth.stigen@kartverket.no>
2022-05-31 13:43:34 +0200

    NIBAS-397: Tilbakemeldinger etter QA

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,8 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenLocal()
+    mavenCentral()
 }
 
 object DependencyVersions {
