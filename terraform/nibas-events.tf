21eb61c8353f2e8e4b6976dabb7a989ef4a9237e
Kenneth Stigen <kenneth.stigen@kartverket.no>
2022-06-13 14:30:08 +0200

    NIBAS-465: Oppsett av kafka

--- terraform/nibas-events.tf
+++ terraform/nibas-events.tf
@@ -1,19 +1,3 @@
-terraform {
-  backend "gcs" {
-    prefix = "nibas-events"
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-locals {
-  namespace = "nibas"
-}
-
-variable "nibas_events_version" {}
-variable "ENVIRONMENT" {}
 
 resource "kubernetes_deployment" "nibas-events-deployment" {
   metadata {
