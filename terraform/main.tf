ad323a0c89b954bd87b49af2403af2929f477cce
Kenneth Stigen <kenneth.stigen@kartverket.no>
2022-08-23 14:03:21 +0200

    NIBAS-352: Håndterer database selv

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,13 @@ terraform {
   backend "gcs" {
     prefix = "nibas-events"
   }
+
+  required_providers {
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.17.1"
+    }
+  }
 }
 
 provider "kubernetes" {
@@ -12,5 +19,22 @@ locals {
   namespace = "nibas"
 }
 
+variable "vault_addr" {}
+variable "vault_skip_verify" {}
+variable "GITHUB_TOKEN" {sensitive = true}
+
 variable "nibas_events_version" {}
 variable "ENVIRONMENT" {}
+
+provider "vault" {
+  address = var.vault_addr
+  skip_tls_verify = var.vault_skip_verify
+  auth_login {
+    path = "auth/jwt-dev/login"
+    method = "jwt"
+    parameters = {
+      role = "nibas-events-write-role"
+      jwt = var.GITHUB_TOKEN
+    }
+  }
+}
