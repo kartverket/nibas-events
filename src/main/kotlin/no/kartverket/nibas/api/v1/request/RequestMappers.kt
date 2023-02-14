3e15b3fd1b03b32dca33054df3cce755c1e79b4d
soramu <amund.sorumshagen@kartverket.no>
2023-02-14 17:07:37 +0100

    TS-856 Småfiks og modellendringer

--- src/main/kotlin/no/kartverket/nibas/api/v1/request/RequestMappers.kt
+++ src/main/kotlin/no/kartverket/nibas/api/v1/request/RequestMappers.kt
@@ -4,13 +4,13 @@ import no.kartverket.nibas.domain.Event
 import no.kartverket.nibas.domain.EventRad
 import no.kartverket.nibas.domain.EventType
 import no.kartverket.nibas.domain.FlateType
-import java.time.LocalDateTime
+import java.time.OffsetDateTime
 
 fun EventRequest.toEvent(): Event {
     return Event(
         id = 0, // en @Id som er null eller 0 vil tolkes som new fra Spring Data JDBC, og dermed telles opp
         inntreffer = inntreffer,
-        timestamp = LocalDateTime.now(),
+        timestamp = OffsetDateTime.now(),
         eventRader = this.eventRader.map {
             EventRad(
                 uuid = it.uuid,
