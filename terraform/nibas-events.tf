56ff79058f080b908932adbbdcaf70ddae6720b7
Steffen Hageland <steffen.hageland@bekk.no>
2023-04-27 15:45:53 +0200

    Terraform format

--- terraform/nibas-events.tf
+++ terraform/nibas-events.tf
@@ -88,11 +88,11 @@ resource "kubernetes_manifest" "nibas_events_application" {
           rules = [
             {
               application = "nibas-backend"
-              namespace = "nibas"
+              namespace   = "nibas"
             },
             {
               application = "nibas-backend"
-              namespace = "nibas-main"
+              namespace   = "nibas-main"
             },
             {
               application = "dataplattform-proxy"
