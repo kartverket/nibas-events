c952b2a84870319a70cacac1009ef9416e85d2f3
Anders Østen <ostean@kartverket.no>
2023-10-16 15:11:20 +0200

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version kotlinPluginVersion
     kotlin("plugin.spring") version kotlinPluginVersion
-    id("org.flywaydb.flyway") version "9.22.3"
+    id("org.flywaydb.flyway") version "9.22.1"
 }
 
 group = "no.kartverket"
