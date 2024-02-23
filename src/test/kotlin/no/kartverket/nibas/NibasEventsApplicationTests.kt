eac8be00574cad0f910e52ecf3a990be028908f1
Anders Østen <ostean@kartverket.no>
2024-02-23 09:33:39 +0100

    TS-1509: Oppgraderer java, gradle og avhengigheter - Java 21, gradle 8.6 - Har også fjernet detekt - For å komme oss rundt problemet med lukkede moduler (https://docs.gradle.org/current/userguide/upgrading_version_7.html#remove_test_add_opens) var løsningen å bruke WebEnvironment.MOCK i TestWithDbContainer

--- src/test/kotlin/no/kartverket/nibas/NibasEventsApplicationTests.kt
+++ src/test/kotlin/no/kartverket/nibas/NibasEventsApplicationTests.kt
@@ -2,9 +2,7 @@ package no.kartverket.nibas
 
 import no.kartverket.nibas.inttest.TestWithDbContainer
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class NibasEventsApplicationTests : TestWithDbContainer() {
 
 	@Test
