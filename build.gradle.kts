03d14ac7434ada3aa9f2203344e89d40a5a61139
Jorunn Toft <tofjor@kartverket.no>
2024-08-22 10:28:24 +0200

    TS-1895: Bumper også prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ val SPRINGDOC_OPENAPI_VERSION2 = "2.5.0"
 val LOGSTASH_VERSION = "8.0"
 val FLYWAY_VERSION = "10.17.1"
 val TEST_CONTAINER_VERSION = "1.20.1"
-val PROMETHEUS_VERSION = "1.12.5"
+val PROMETHEUS_VERSION = "1.13.3"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
